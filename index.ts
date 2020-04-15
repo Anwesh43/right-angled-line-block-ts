0cd62d7aa15156705153a571731e7910e276ce97
Anwesh43 <anweshthecool0@gmail.com>
2020-04-16 01:55:01 +0530

    adding RightAngledLine class

--- index.ts
+++ index.ts
@@ -173,3 +173,27 @@ class RALNode {
         return this
     }
 }
+
+class RightAngledLine {
+
+    root : RALNode = new RALNode(0)
+    curr : RALNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
