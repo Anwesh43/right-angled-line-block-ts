038978aaae5a4913772b5e1e79295d78d740d7f9
Anwesh43 <anweshthecool0@gmail.com>
2020-04-16 01:52:06 +0530

    adding RALNode class

--- index.ts
+++ index.ts
@@ -131,3 +131,45 @@ class Animator {
         }
     }
 }
+
+class RALNode {
+
+    prev : RALNode
+    next : RALNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new RALNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawRALNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : RALNode {
+        var curr : RALNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
